b859a29c8e3b0994da957a07e93b009635d2c781
Cuauhtemoc Guerrero Hernandez <C0497660@intra.camosun.bc.ca>
2023-05-05 01:51:54 -0700

    Working for one rate deck set.

--- new_import_V2.py
+++ new_import_V2.py
@@ -4,7 +4,7 @@ import pandas as pd
 def dataframe(files):
     dataframe = pd.DataFrame([])
     for f in files:
-        df = pd.read_csv(f'{f}', converters={ 'prefix': str, 'rate': float})
+        df = pd.read_csv(f'{f}', converters={'prefix': str, 'rate': float})
         df['carrier'] = '#' + os.path.splitext(f)[0]
         dataframe = pd.concat([dataframe, df])
     return dataframe
@@ -12,33 +12,40 @@ def dataframe(files):
 at_df = dataframe(['Avid.csv', 'Touchtone.csv'])
 cn_df = dataframe(['CCI.csv', 'Novatel.csv'])
 
-# filter rates below 0.004 (cci & novatel)
+# Filter rates below 0.004 for cci & novatel
 filtered_under_df = cn_df[cn_df['rate'] < 0.004]
-# filter rates above or equal to 0.004 (cci & novatel)
+
+# Find cheapest carriers for each prefix in filtered_under_df
+cheapest_under_df = filtered_under_df.sort_values(['rate']).groupby('prefix')['carrier'].apply(', '.join).to_frame()
+
+# Filter rates above 0.004 for cci & novatel
 filtered_over_df = cn_df[cn_df['rate'] >= 0.004]
 
-# join filtered_under_df and at_df
-merged1_df = pd.concat([filtered_under_df, at_df])
-# sort merged1_df by rate and group by prefix and carrier
-grouped1_df = merged1_df.sort_values(['rate']).groupby(['prefix', 'carrier'], sort=False)['rate'].min().reset_index()
-# sort grouped1_df by rate and group by prefix
-grouped1_df = grouped1_df.sort_values(['rate', 'carrier']).groupby('prefix', sort=False)['carrier'].apply(', '.join).to_frame()
+# Merge filtered_over_df and at_df
+merged_over_df = pd.concat([filtered_over_df, at_df])
 
-# sort filtered_over_df by rate and group by prefix and carrier
-grouped2_df = filtered_over_df.sort_values(['rate']).groupby(['prefix', 'carrier'], sort=False)['rate'].min().reset_index()
-# sort grouped2_df by rate and group by prefix
-grouped2_df = grouped2_df.sort_values(['rate', 'carrier']).groupby('prefix', sort=False)['carrier'].apply(', '.join).to_frame()
+# Sort by prefix and rate
+#merged_over_df = merged_over_df.sort_values(['prefix', 'rate'])
 
-# concatenate grouped1_df and grouped2_df
-result_df = pd.concat([grouped1_df, grouped2_df], sort=False)
-result_df['ruleid'] = range(1, 1+len(result_df))
-result_df['groupid'] = 3
-result_df['timerec'] = None
-result_df['priority'] = 0
-result_df['routeid'] = None
-result_df['sort_alg'] = 'N'
-result_df['sort_profile'] = None
-result_df['attrs'] = None
-result_df['description'] = 'LCR'
-result_df.rename(columns={'carrier': 'gwlist'}, inplace=True)
-result_df.to_csv('result.csv', index=False)
+# Find cheapest carriers for each prefix
+cheapest_over_df = merged_over_df.sort_values(['rate']).groupby('prefix')['carrier'].apply(', '.join).to_frame()
+
+# Merge cheapest_under_df and cheapest_over_df
+merged_df = pd.merge(cheapest_under_df, cheapest_over_df, on='prefix', how='outer', suffixes=('_under', '_over'))
+merged_df['gwlist'] = merged_df['carrier_under'].fillna('') + ', ' + merged_df['carrier_over'].fillna('')
+merged_df = merged_df.drop(['carrier_under', 'carrier_over'], axis=1)
+merged_df.to_csv('merged.csv')
+
+#merged_df = merged_df.append({'prefix': prefix, 'carrier_under': ','.join(cheapest_under), 'carrier_over': ','.join(cheapest_over), 'gwlist': carriers}, ignore_index=True)
+
+merged_df['ruleid'] = range(1, 1 + len(merged_df))
+merged_df['groupid'] = 3
+merged_df['timerec'] = None
+merged_df['priority'] = 0
+merged_df['routeid'] = None
+merged_df['sort_alg'] = 'N'
+merged_df['sort_profile'] = None
+merged_df['attrs'] = None
+merged_df['description'] = 'LCR'
+merged_df.rename(columns={'carrier': 'gwlist'}, inplace=True)
+merged_df.to_csv('result.csv')
